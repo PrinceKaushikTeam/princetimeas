cbf47682c5a94028b9efcc09184c1a68330985b1
PrinceKaushikTeam <92310155+PrinceKaushikTeam@users.noreply.github.com>
2021-10-12 16:29:52 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,17 +1,11 @@
-download_ngrok() {
-	url="$1"
-	file=`basename $url`
-	if [[ -e "$file" ]]; then
-		rm -rf "$file"
-	fi
-	wget --no-check-certificate "$url" > /dev/null 2>&1
-	if [[ -e "$file" ]]; then
-		unzip "$file" > /dev/null 2>&1
-		mv -f ngrok .server/ngrok > /dev/null 2>&1
-		rm -rf "$file" > /dev/null 2>&1
-		chmod +x .server/ngrok > /dev/null 2>&1
-	else
-		echo -e "\n${RED}[${WHITE}!${RED}]${RED} Error occured, Install Ngrok manually."
-		{ reset_color; exit 1; }
-	fi
-}
+install_ngrok()
+{
+if [ -e $PREFIX/bin/curl ];
+then
+	curl -LO https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+	apt install unzip
+	unzip ngrok-stable-linux-arm.zip
+	mv ngrok $PREFIX/bin/
+	chmod +x $PREFIX/bin/ngrok
+	echo
+	echo -e "$green Ngrok is installed :)"
